d8688e43ecf27dec70a8daba5454847b947ae1de
andrew blinn <me@disconcision.com>
2018-07-07 00:17:39 -0700

    finished proof-of-concept write-in-envs

--- write-in-envs.rkt
+++ write-in-envs.rkt
@@ -10,48 +10,55 @@
 
 (define p2
   '(◇
-    (p/
-     #hash((sort . expr))
-     (λ ((p/ #hash((sort . pat) (▹ . ▹)) (var (p/ #hash((sort . char)) name))))
-       (p/ #hash((sort . expr)) ⊙)))))
-
-(define p3
-  '(◇
-    (p/
-     #hash((sort . expr))
-     (app
-      (p/
-       #hash((sort . expr)
-             (▹ . ▹))
-       ⊙)
-      (p/
-       #hash((sort . expr))
-       ⊙)))))
+    (p/ #hash((sort . expr))
+        (λ ((p/ #hash((sort . pat) (▹ . ▹)) (var (p/ #hash((sort . char)) name))))
+          (p/ #hash((sort . expr))
+              (λ ((p/ #hash((sort . pat) (▹ . ▹)) (var (p/ #hash((sort . char)) name2))))
+                (p/ #hash((sort . expr))
+                    (app (p/ #hash((sort . expr) (▹ . ▹)) ⊙)
+                         (p/ #hash((sort . expr)) ⊙)))))))))
 
 
-(define (write-in-envs env stx)
-  (define W (curry write-in-envs env))
+(module+ test
+  (require rackunit)
+  (check-equal?
+   (write-in-envs p2 )
+   '(◇ (p/
+        #hash((in-scope . ()) (sort . expr))
+        (λ ((p/ #hash((sort . pat) (▹ . ▹)) (var (p/ #hash((sort . char)) name))))
+          (p/ #hash((in-scope . (name)) (sort . expr))
+              (λ ((p/ #hash((sort . pat) (▹ . ▹)) (var (p/ #hash((sort . char)) name2))))
+                (p/ #hash((in-scope . (name2 name)) (sort . expr))
+                    (app (p/ #hash((in-scope . (name2 name)) (sort . expr) (▹ . ▹)) ⊙)
+                         (p/ #hash((in-scope . (name2 name)) (sort . expr)) ⊙))))))))))
+
+
+
+
+(define (write-in-envs stx)
+  (define W (curry write-in-envs))
   (match stx
-    [`(◇ (p/ ,top-hash ,prog))
-     `(◇ (p/ ,(hash-set top-hash 'in-scope env) ,prog))]
-    [`(λ ((p/ ,a (var (p/ ,b ,id))))
-        (p/ ,body-anns ,body))
-     (define new-env `(,id ,@env))
-     `(λ ((p/ ,a (var (p/ ,b ,id))))
-        (p/ ,(hash-set body-anns 'in-scope new-env) ,(W body)))]
+    
+    [`(◇ (p/ ,anns ,prog))
+     `(◇ ,(W `(p/ ,(hash-set anns 'in-scope '()) ,prog)))]
 
-
-    [`(app (p/
-            ,a
-            ,f-expr)
-           (p/
-            ,b
-            ,a-expr))
-     `(app (p/
-            ,(hash-set a)
-            ,f-expr)
-           (p/
-            ,b
-            ,a-expr))]))
+    [`(p/ ,anns ⊙)
+     `(p/ ,anns ⊙)]
+    
+    [`(p/ ,(and top-hash (hash-table ('in-scope env)))
+          (λ ((p/ ,a (var (p/ ,b ,id))))
+            (p/ ,body-anns
+                ,body)))
+     `(p/ ,top-hash
+          (λ ((p/ ,a (var (p/ ,b ,id))))
+            ,(W `(p/ ,(hash-set body-anns 'in-scope `(,id ,@env))
+                     ,body))))]
+    
+    [`(p/ ,(and top-hash (hash-table ('in-scope env)))
+          (app (p/ ,a ,f-expr)
+               (p/ ,b ,a-expr)))
+     `(p/ ,top-hash
+          (app ,(W `(p/ ,(hash-set a 'in-scope env) ,f-expr))
+               ,(W `(p/ ,(hash-set b 'in-scope env) ,a-expr))))]))
 
 
