221543162a311e5590e47d4dfb034dad5d534a29
andrew blinn <me@disconcision.com>
2018-11-10 23:40:48 -0500

    pointless fiddling

--- choice-stepper.rkt
+++ choice-stepper.rkt
@@ -1,49 +1,51 @@
 #lang racket
 
+; andrew blinn 2018
+
 (provide step-choice)
 
 
 ; tests commented out to supress printing
-#; (module+ test
-     (require rackunit)
-     (check-equal? (step-choice 0)
-                   '(0))
-     (check-equal? (step-choice '(list))
-                   '(()))
-     (check-equal? (step-choice '(list 1 2 3))
-                   '((1 2 3)))
-     (check-equal? (step-choice '(list 1 (list 2 3) 4))
-                   '((1 (2 3) 4)))
 
-     (check-equal? (step-choice '(next))
-                   '())
+#;(module+ test
+    (require rackunit)
+    (check-equal? (step-choice 0)
+                  '(0))
+    (check-equal? (step-choice '(list))
+                  '(()))
+    (check-equal? (step-choice '(list 1 2 3))
+                  '((1 2 3)))
+    (check-equal? (step-choice '(list 1 (list 2 3) 4))
+                  '((1 (2 3) 4)))
 
-     (check-equal? (step-choice '(-<))
-                   '())
-     (check-equal? (step-choice '(-< 1))
-                   '(1))
-     (check-equal? (step-choice '(-< 1 2 3))
-                   '(1 2 3))
+    (check-equal? (step-choice '(next))
+                  '())
 
-     (check-equal? (step-choice '(list 0 (-< 1 2 3)))
-                   '((0 1) (0 2) (0 3)))
-     (check-equal? (step-choice '(list (-< 1 2) (-< 3 4)))
-                   '((1 3) (1 4) (2 3) (2 4)))
-     (check-equal? (step-choice '(-< (list 1 2) 3))
-                   '((1 2) 3))
-     (check-equal? (step-choice '(-< (list (-< 1 2)) 3))
-                   '((1) (2) 3))
+    (check-equal? (step-choice '(-<))
+                  '())
+    (check-equal? (step-choice '(-< 1))
+                  '(1))
+    (check-equal? (step-choice '(-< 1 2 3))
+                  '(1 2 3))
 
-     (check-equal? (step-choice '(list (next)))
-                   '())
-     (check-equal? (step-choice '(list (-<)))
-                   '())
+    (check-equal? (step-choice '(list 0 (-< 1 2 3)))
+                  '((0 1) (0 2) (0 3)))
+    (check-equal? (step-choice '(list (-< 1 2) (-< 3 4)))
+                  '((1 3) (1 4) (2 3) (2 4)))
+    (check-equal? (step-choice '(-< (list 1 2) 3))
+                  '((1 2) 3))
+    (check-equal? (step-choice '(-< (list (-< 1 2)) 3))
+                  '((1) (2) 3))
 
-     (check-equal? (step-choice '(-< (-< (next) 2) 3))
-                   '(2 3))
-     (check-equal? (step-choice '(-< (list (-< 1 2) (next)) 3))
-                   '(3))
-     )
+    (check-equal? (step-choice '(list (next)))
+                  '())
+    (check-equal? (step-choice '(list (-<)))
+                  '())
+
+    (check-equal? (step-choice '(-< (-< (next) 2) 3))
+                  '(2 3))
+    (check-equal? (step-choice '(-< (list (-< 1 2) (next)) 3))
+                  '(3)))
 
 
 (define (expr!? stx)
@@ -64,27 +66,22 @@
     [(? number?) #t]
     ['() #t]
     ['done #t]
-    [`(quote ,_) #t]
     ['(next) #f]
     [`(list ,xs ...) #f]
     [`(-< ,xs ...) #f]
-    [`(▹ ,x) #f]
+    #;[`(▹ ,x) #f]
     [(? list?) (andmap value? stx)]))
 
 
-(define (step state stack)
+(define (step stx stack)
   ; small-steps state where ▹ indicates current redex
   
-  (define new-state
-    (match state
+  (define new-stx
+    (match stx
       [(or (⋱ c `(▹ (next)))
            (⋱ c `(▹ (-<))))
        (if (empty? stack)
-           'done
-           (first stack))]
-      [(⋱ c `(▹ (-<)))
-       (if (empty? stack)
-           'done
+           `(▹ done)
            (first stack))]
       [(⋱ c `(▹ (-< ,a)))
        (⋱ c `(▹ ,a))]
@@ -92,19 +89,19 @@
        (⋱ c `(▹ (-< ,a)))]
       [(⋱ c `(▹ (list)))
        (⋱ c `(▹ ()))]
-      [(⋱ c `(▹ (list ,x ,ys ...)))
-       (⋱ c `(list (▹ ,x) ,@ys))]
+      [(⋱ c `(▹ (list ,x ,xs ...)))
+       (⋱ c `(list (▹ ,x) ,@xs))]
       [(⋱ c `(list ,xs ... (▹ ,(? value? x)) ,y ,ys ...))
        (⋱ c `(list ,@xs ,x (▹ ,y) ,@ys))]
       [(⋱ c `(list ,xs ... (▹ ,(? value? x))))
        (⋱ c `(▹ (,@xs ,x)))]))
 
   (define new-stack
-    (match state
+    (match stx
       [(or (⋱ c `(▹ (next)))
            (⋱ c `(▹ (-<))))
        (if (empty? stack)
-           'done
+           '()
            (begin
              (println `(pop-stack))
              (rest stack)))]
@@ -112,40 +109,34 @@
        stack]
       [(⋱ c `(▹ (-< ,a ,as ...)))
        (println `(push-stack: ,(⋱ c `(▹ (-< ,@as)))))
-       (cons (⋱ c `(▹ (-< ,@as))) stack)]
+       `(,(⋱ c `(▹ (-< ,@as))) ,@stack)]
       [_ stack]))
 
-  (values new-state new-stack))
+  (values new-stx new-stack))
 
 
 (define (step-until-value stx stack)
+  ; steps stx/stack until stx is a value and the stack is empty
   (println stx)
   (define fully-evaluated?
-    (disjoin (curry equal? 'done)
+    (disjoin (match-lambda? `(▹ done))
              (match-lambda? `(▹ ,(? value?)))))
-  (if (fully-evaluated? stx)
-      (if (or (empty? stack)
-              (equal? stx 'done))
-          `(,stx)
-          (begin (println `(pop-stack-auto))
-                 `(,stx ,@(step-until-value (first stack)
-                                            (rest stack)))))
+  (if (not (fully-evaluated? stx))
       (let-values ([(new-stx new-stack) (step stx stack)])
-        
-        (step-until-value new-stx new-stack))))
+        (step-until-value new-stx new-stack))
+      (match-let ([`(▹ ,bare-stx) stx])
+        (cond
+          [(equal? bare-stx 'done) '()]
+          [(empty? stack) `(,bare-stx)]
+          [else (println `(pop-stack-auto))
+                `(,bare-stx ,@(step-until-value (first stack)
+                                                (rest stack)))]))))
 
 
 (define (step-choice stx)
+  ; returns a list of all results from evaluating stx
   (expr!? stx)
-  (define redex-stx `(▹ ,stx))
-  (define initial-stack '())
-  (define results (step-until-value redex-stx initial-stack))
-  (for/fold ([acc '()])
-            ([r results])
-    (match r
-      [`(▹ ,x) `(,@acc ,x)]
-      ['done acc]
-      [x x])))
+  (step-until-value `(▹ ,stx) '()))
 
 
 
@@ -154,7 +145,8 @@
 ; ------------------------------
 
 ; this is library code to implement context patterns
-; dumped here so to avoid the need for additional requires
+; dumped here to make this standalone
+; (see containment-patterns on my github)
 
 (define-match-expander ⋱
   ; containment pattern (returns first match)
@@ -175,7 +167,6 @@
 (define-syntax-rule (match-lambda? <pat>)
   ; converts a pattern into a predicate
   (match-lambda [<pat> #t] [_ #f]))
-
 
 
 (define (multi-split ls lengths)
@@ -234,4 +225,5 @@
     (multi-containment match? xs until?))
   (if (empty? matches)
       (list context matches)
-      (list (λ (x) (apply context x (rest matches))) (list (first matches)))))
+      (list (λ (x) (apply context x (rest matches)))
+            (list (first matches)))))
